dad1bb1fde658b88e761fecd856c6234b90f16d5
AngieJoseth <angiecsb982@gmail.com>
2019-05-06 18:03:07 -0500

    Fondo

--- app/src/Home/View.fs
+++ app/src/Home/View.fs
@@ -4,6 +4,7 @@ open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
 let root =
-  div
-    [ ]
-    [ ]
+  div [ ClassName "content"]
+          [ figure  [ ClassName "image is-2by1"]
+                  [ img  [ Src "img/FondoPagina.png"] ] ] 
+        
